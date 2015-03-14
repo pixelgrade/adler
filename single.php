e7e4a00a423548bc0af4a3fb83db988f6c738374
Constantin Radu <constantinradu@Constantins-MacBook-Pro.local>
2015-03-14 13:55:38 +0200

    Hero added #1

--- single.php
+++ single.php
@@ -10,6 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<?php while ( have_posts() ) : the_post();
+            get_template_part( 'templates/single/hero');
             get_template_part( 'content', 'single' );
             the_post_navigation();
                 // If comments are open or we have at least one comment, load up the comment template
