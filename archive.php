245ecc59d10c078f208e5740f1528ee28c2e009e
Alexandru <allexsavafabian@gmail.com>
2015-03-26 03:16:20 +0200

    Category Archive #18

--- archive.php
+++ archive.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The main template file.
  *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package The Adler
@@ -10,17 +14,22 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-	<?php get_template_part( 'templates/archive/hero' ); ?>
+
 	<main id="main" class="site-main" role="main">
 		<?php
 		$counter = 0;
 		if ( have_posts() ) : while ( have_posts() ) : the_post();
-			//The hero is already previewing the latest post. Excluding from the archive
-			if ($counter++ < 1) {
+			if ( has_post_thumbnail($posts[0]->ID) && ($counter == 0)) {
+				get_template_part( 'content', 'hero' );
+				$counter++;
 				continue;
 			}
-
-			get_template_part( 'content', get_post_format() );
+			if ($counter%2 == 1) {
+				get_template_part( 'content', 'odd' );
+			} else {
+				get_template_part( 'content', 'even' );
+			}
+			$counter++;
 		endwhile;
 			the_posts_navigation();
 		else : get_template_part( 'content', 'none' );  endif;
@@ -28,8 +37,6 @@ get_header(); ?>
 	</main>
 	<!-- #main -->
 </div><!-- #primary -->
-
-
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
