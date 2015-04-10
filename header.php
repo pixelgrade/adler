defb03b6b77c4bd23ed9592c06e56ebe8bf1a201
Alexandru <allexsavafabian@gmail.com>
2015-04-10 18:15:10 +0300

    SCSS+PHP Fixes

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site <?php ( has_post_thumbnail() ) ? 'has-thumbnail' : 'no-thumbnail'; ?>">
+<div id="page" class="site <?php echo ( has_post_thumbnail() ) ? 'has-thumbnail' : 'no-thumbnail'; ?>">
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<a class="logo" href="<?php echo home_url(); ?>">
