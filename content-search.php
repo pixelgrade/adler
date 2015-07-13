950ed9caa9291068514d800f6a34ab0ed3b0f684
Robert <BAsusrog750>
2015-07-13 16:18:27 +0300

    fixes

--- content-search.php
+++ content-search.php
@@ -11,7 +11,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<blockquote class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></blockquote>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 
